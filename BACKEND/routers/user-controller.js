3cc827e1074a401f448411fbaba2ca255e1119bf
Kanha Rathod <rathodkanha8810@gmail.com>
2024-10-05 16:36:45 +0530

    working on user

--- BACKEND/routers/user-controller.js
+++ BACKEND/routers/user-controller.js
@@ -1,191 +1,269 @@
-const connection = require('./db-connection.js');
-const bcrypt = require('bcryptjs');
+const connection = require("./db-connection.js");
+const bcrypt = require("bcryptjs");
+const getUserIdFromToken = require("./utils");
 
 // Add User
 exports.addUser = (req, res) => {
-    const { first_name, last_name, mobile_number, email, password, role, permission } = req.body;
-    if (!first_name || !last_name || !mobile_number || !email || !password || !role || !permission) {
-        return res.status(400).json({ success: false, message: 'All fields are required' });
+  const { first_name, last_name, mobile_number, email, password, role } =
+    req.body;
+  if (
+    !first_name ||
+    !last_name ||
+    !mobile_number ||
+    !email ||
+    !password ||
+    !role
+  ) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  const userId = getUserIdFromToken(token);
+
+  if (!userId) {
+    return res.status(403).json({ success: false, message: "Invalid token" });
+  }
+  const checkUserQuery =
+    "SELECT email FROM users WHERE email = ? AND status = ?";
+  connection.db.execute(checkUserQuery, [email, "ACTIVE"], (err, results) => {
+    if (err) {
+      console.error("Database query error:", err);
+      return res
+        .status(500)
+        .json({ success: false, message: "Database query error" });
     }
 
-    const checkUserQuery = 'SELECT email FROM users WHERE email = ? AND status = ?';
-    connection.db.execute(checkUserQuery, [email, 'ACTIVE'], (err, results) => {
+    if (results.length > 0) {
+      return res.status(400).json({
+        success: false,
+        message: "User with this email already exists",
+      });
+    }
+    const insertUserQuery =
+      "INSERT INTO users (first_name, last_name, mobile_number, email, password, role, created_at, status, createdBy) VALUES (?, ?, ?, ?, ?, ?, NOW(), ?, ?)";
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    connection.db.execute(
+      insertUserQuery,
+      [
+        first_name,
+        last_name,
+        mobile_number,
+        email,
+        hashedPassword,
+        role,
+        "ACTIVE",
+        userId,
+      ],
+      (err, results) => {
         if (err) {
-            console.error('Database query error:', err);
-            return res.status(500).json({ success: false, message: 'Database query error' });
+          console.error("Error registering user:", err);
+          return res
+            .status(500)
+            .json({ success: false, message: "Error registering user" });
         }
 
-        if (results.length > 0) {
-            return res.status(400).json({ success: false, message: 'User with this email already exists' });
-        }
-        const insertUserQuery = 'INSERT INTO users (first_name, last_name, mobile_number, email, password, role, created_at, status, permission) VALUES (?, ?, ?, ?, ?, ?, NOW(), ?, ?)';
-        const hashedPassword = bcrypt.hashSync(password, 8);
-        connection.db.execute(insertUserQuery, [first_name, last_name, mobile_number, email, hashedPassword, role, 'ACTIVE', permission], (err, results) => {
-            if (err) {
-                console.error('Error registering user:', err);
-                return res.status(500).json({ success: false, message: 'Error registering user' });
-            }
-
-            res.status(201).json({ success: true, message: 'User registered successfully' });
-        });
-    });
-}
+        res
+          .status(201)
+          .json({ success: true, message: "User registered successfully" });
+      }
+    );
+  });
+};
 
 // Get active users list
 exports.getUserList = (req, res) => {
-    let { page, keyword, date_from, date_to } = req.query;
+  let { page, keyword, date_from, date_to } = req.query;
 
-    const limit = 10;
-    page = page ? parseInt(page, 10) : 1;
+  const limit = 10;
+  page = page ? parseInt(page, 10) : 1;
 
-    if (isNaN(page) || page < 1) {
-        return res.status(400).json({ success: false, message: 'Invalid page value' });
-    }
+  if (isNaN(page) || page < 1) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid page value" });
+  }
 
-    const offset = (page - 1) * limit;
+  const offset = (page - 1) * limit;
 
-    let query = `
+  let query = `
         SELECT id, first_name, last_name, CONCAT(first_name, ' ', last_name) AS name, 
-               mobile_number, email, role, permission, created_at
+               mobile_number, email, role, created_at
         FROM users 
         WHERE status = 'ACTIVE' AND isCustomer = false`;
 
-    let countQuery = `SELECT COUNT(*) AS total FROM users WHERE status = 'ACTIVE' AND isCustomer = false`;
+  let countQuery = `SELECT COUNT(*) AS total FROM users WHERE status = 'ACTIVE' AND isCustomer = false`;
 
-    if (date_from && date_to) {
-        query += ` AND DATE(created_at) BETWEEN DATE('${date_from}') AND DATE('${date_to}')`;
-        countQuery += ` AND DATE(created_at) BETWEEN DATE('${date_from}') AND DATE('${date_to}')`;
-    } else if (date_from) {
-        query += ` AND DATE(created_at) >= DATE('${date_from}')`;
-        countQuery += ` AND DATE(created_at) >= DATE('${date_from}')`;
-    } else if (date_to) {
-        query += ` AND DATE(created_at) <= DATE('${date_to}')`;
-        countQuery += ` AND DATE(created_at) <= DATE('${date_to}')`;
+  if (date_from && date_to) {
+    query += ` AND DATE(created_at) BETWEEN DATE('${date_from}') AND DATE('${date_to}')`;
+    countQuery += ` AND DATE(created_at) BETWEEN DATE('${date_from}') AND DATE('${date_to}')`;
+  } else if (date_from) {
+    query += ` AND DATE(created_at) >= DATE('${date_from}')`;
+    countQuery += ` AND DATE(created_at) >= DATE('${date_from}')`;
+  } else if (date_to) {
+    query += ` AND DATE(created_at) <= DATE('${date_to}')`;
+    countQuery += ` AND DATE(created_at) <= DATE('${date_to}')`;
+  }
+
+  let queryParams = [];
+  let countParams = [];
+
+  if (keyword) {
+    query += ` AND (CONCAT(first_name, ' ', last_name) LIKE ? OR email LIKE ?)`;
+    countQuery += ` AND (CONCAT(first_name, ' ', last_name) LIKE ? OR email LIKE ?)`;
+    queryParams.push(`%${keyword}%`, `%${keyword}%`);
+    countParams.push(`%${keyword}%`, `%${keyword}%`);
+  }
+
+  query += ` LIMIT ${limit} OFFSET ${offset}`;
+
+  connection.db.execute(query, queryParams, (err, results) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Database query error" });
     }
 
-    let queryParams = [];
-    let countParams = [];
+    connection.db.execute(countQuery, countParams, (err, countResults) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ success: false, message: "Database query error" });
+      }
 
-    if (keyword) {
-        query += ` AND (CONCAT(first_name, ' ', last_name) LIKE ? OR email LIKE ?)`;
-        countQuery += ` AND (CONCAT(first_name, ' ', last_name) LIKE ? OR email LIKE ?)`;
-        queryParams.push(`%${keyword}%`, `%${keyword}%`);
-        countParams.push(`%${keyword}%`, `%${keyword}%`);
-    }
+      const totalRecords = countResults[0].total;
+      const totalPages = Math.ceil(totalRecords / limit);
 
-    query += ` LIMIT ${limit} OFFSET ${offset}`;
-
-    connection.db.execute(query, queryParams, (err, results) => {
-        if (err) {
-            return res.status(500).json({ success: false, message: 'Database query error' });
-        }
-
-        connection.db.execute(countQuery, countParams, (err, countResults) => {
-            if (err) {
-                return res.status(500).json({ success: false, message: 'Database query error' });
-            }
-
-            const totalRecords = countResults[0].total;
-            const totalPages = Math.ceil(totalRecords / limit);
-
-            res.status(200).json({
-                success: true,
-                data: results,
-                pagination: {
-                    totalRecords,
-                    totalPages,
-                    currentPage: page,
-                    pageSize: limit,
-                },
-            });
-        });
+      res.status(200).json({
+        success: true,
+        data: results,
+        pagination: {
+          totalRecords,
+          totalPages,
+          currentPage: page,
+          pageSize: limit,
+        },
+      });
     });
+  });
 };
-
 
 // Update active user
 exports.updateUser = (req, res) => {
-    const { userId } = req.params;
-    const { first_name, last_name, mobile_number, email, role, permission, password } = req.body;
+  const { userId } = req.params;
+  const { first_name, last_name, mobile_number, email, role, password } =
+    req.body;
 
-    if (!first_name || !last_name || !mobile_number || !email || !role || !permission) {
-        return res.status(400).json({ success: false, message: 'All fields are required' });
-    }
+  if (!first_name || !last_name || !mobile_number || !email || !role) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  const loginUserId = getUserIdFromToken(token);
 
-    const updateFields = [];
-    const queryParams = [];
+  if (!loginUserId) {
+    return res.status(403).json({ success: false, message: "Invalid token" });
+  }
 
-    if (first_name) {
-        updateFields.push('first_name = ?');
-        queryParams.push(first_name);
-    }
-    if (last_name) {
-        updateFields.push('last_name = ?');
-        queryParams.push(last_name);
-    }
-    if (mobile_number) {
-        updateFields.push('mobile_number = ?');
-        queryParams.push(mobile_number);
-    }
-    if (email) {
-        updateFields.push('email = ?');
-        queryParams.push(email);
-    }
-    if (role) {
-        updateFields.push('role = ?');
-        queryParams.push(role);
-    }
-    if (permission) {
-        updateFields.push('permission = ?');
-        queryParams.push(permission);
-    }
-    if (password && password.length > 0) {
-        updateFields.push('password = ?');
-        const hashedPassword = bcrypt.hashSync(password, 8);
-        queryParams.push(hashedPassword);
-    }
+  const updateFields = [];
+  const queryParams = [];
 
-    queryParams.push(userId);
+  if (first_name) {
+    updateFields.push("first_name = ?");
+    queryParams.push(first_name);
+  }
+  if (last_name) {
+    updateFields.push("last_name = ?");
+    queryParams.push(last_name);
+  }
+  if (mobile_number) {
+    updateFields.push("mobile_number = ?");
+    queryParams.push(mobile_number);
+  }
+  if (email) {
+    updateFields.push("email = ?");
+    queryParams.push(email);
+  }
+  if (role) {
+    updateFields.push("role = ?");
+    queryParams.push(role);
+  }
+  if (password && password.length > 0) {
+    updateFields.push("password = ?");
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    queryParams.push(hashedPassword);
+  }
+  if (loginUserId) {
+    updateFields.push("createdBy = ?");
+    queryParams.push(loginUserId);
+  }
 
-    if (updateFields.length === 0) {
-        return res.status(400).json({ success: false, message: 'No fields to update' });
-    }
+  queryParams.push(userId);
 
-    const updateUserQuery = `
+  if (updateFields.length === 0) {
+    return res
+      .status(400)
+      .json({ success: false, message: "No fields to update" });
+  }
+
+  const updateUserQuery = `
         UPDATE users 
-        SET ${updateFields.join(', ')}
+        SET ${updateFields.join(", ")}
         WHERE id = ?`;
 
-    connection.db.execute(updateUserQuery, queryParams, (err, results) => {
-        if (err) {
-            return res.status(500).json({ success: false, message: 'Database query error' });
-        }
+  connection.db.execute(updateUserQuery, queryParams, (err, results) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Database query error" });
+    }
 
-        if (results.affectedRows === 0) {
-            return res.status(404).json({ success: false, message: 'User not found' });
-        }
+    if (results.affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
 
-        res.status(200).json({ success: true, message: 'User updated successfully' });
-    });
-}
+    res
+      .status(200)
+      .json({ success: true, message: "User updated successfully" });
+  });
+};
 
 // Delete user endpoint
 exports.deletUser = (req, res) => {
-    const { userId } = req.params;
+  const { userId } = req.params;
 
-    const deleteUserQuery = `UPDATE users SET status = 'DELETED' WHERE id = ?`;
+  const token = req.headers.authorization.split(" ")[1];
+  const loginUserId = getUserIdFromToken(token);
 
-    connection.db.execute(deleteUserQuery, [userId], (err, results) => {
-        if (err) {
-            console.error('Error deleting user:', err);
-            return res.status(500).json({ success: false, message: 'Error deleting user' });
-        }
+  if (!loginUserId) {
+    return res.status(403).json({ success: false, message: "Invalid token" });
+  }
 
-        if (results.affectedRows === 0) {
-            return res.status(404).json({ success: false, message: 'User not found' });
-        }
+  const deleteUserQuery = `UPDATE users SET status = 'DELETED', createdBy = ? WHERE id = ?`;
 
-        res.status(200).json({ success: true, message: 'User deleted successfully' });
-    });
-}
+  connection.db.execute(
+    deleteUserQuery,
+    [loginUserId, userId],
+    (err, results) => {
+      if (err) {
+        console.error("Error deleting user:", err);
+        return res
+          .status(500)
+          .json({ success: false, message: "Error deleting user" });
+      }
 
+      if (results.affectedRows === 0) {
+        return res
+          .status(404)
+          .json({ success: false, message: "User not found" });
+      }
+
+      res
+        .status(200)
+        .json({ success: true, message: "User deleted successfully" });
+    }
+  );
+};
